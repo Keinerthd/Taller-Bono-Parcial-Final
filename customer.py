bca9c54eb172958c86613ab3ca45a2bdd500901a
Keinerthd <keinerthd@uninorte.edu.co>
2025-10-23 21:09:05 +0000

    aa

---
+++ customer.py
@@ -0,0 +1,12 @@
+class Customer:
+    def __init__(self, name, lastname, phone):
+        self.name = name
+        self.lastname = lastname
+        self.phone = phone
+
+
+name = input("Digite el nombre del cliente")
+lastname = input("Digite el apellido")
+phone
+
+Customer()
